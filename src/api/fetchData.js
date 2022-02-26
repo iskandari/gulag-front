1ad22395efb7e854980f63ddcc0d289d8af40b38
Stefan Georg <github@stefangeorg.net>
2022-02-26 22:52:32 +0100

    moved to REACT_APP

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -5,11 +5,15 @@ export default () =>
     const checkStatus = res => (res.status !== 200 ? reject(res) : res.json());
     console.log(process.env.BASE_URL, process.env);
     return Promise.all([
-      fetch(`${process.env.PUBLIC_URL}/camps`).then(checkStatus),
-      fetch(`${process.env.PUBLIC_URL}/camp-activities`).then(checkStatus),
-      fetch(`${process.env.PUBLIC_URL}/camp-regions`).then(checkStatus),
-      fetch(`${process.env.PUBLIC_URL}/camp-types`).then(checkStatus),
-      fetch(`${process.env.PUBLIC_URL}/periods`).then(checkStatus)
+      fetch(`${process.env.REACT_APP_PUBLIC_URL}/camps`).then(checkStatus),
+      fetch(`${process.env.REACT_APP_PUBLIC_URL}/camp-activities`).then(
+        checkStatus
+      ),
+      fetch(`${process.env.REACT_APP_PUBLIC_URL}/camp-regions`).then(
+        checkStatus
+      ),
+      fetch(`${process.env.REACT_APP_PUBLIC_URL}/camp-types`).then(checkStatus),
+      fetch(`${process.env.REACT_APP_PUBLIC_URL}/periods`).then(checkStatus)
     ])
       .then(([camps, activities, regions, types, periods]) =>
         resolve(fromJS({ camps, activities, regions, types, periods })))
